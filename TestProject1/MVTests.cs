fc150fc8f5da707ac4c2051d196a684d27331364
Andreyeyeye <shebanoff.andrei@yandex.ru>
2025-03-19 09:11:26 +0700

    допилены тесты

--- TestProject1/MVTests.cs
+++ TestProject1/MVTests.cs
@@ -1,117 +1,139 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using STPRGR;
 
-
-namespace STPRGR
+namespace STPRGR.Tests
 {
-
-		[TestClass]
-		public class MainViewModelTests
+	[TestClass]
+	public class MainViewModelTests
+	{
+		[TestMethod]
+		public void AddSymbol_AddsDigitToInputText()
 		{
-			[TestMethod]
-			public void AddSymbol_AddsDigitToInputText()
-			{
-				// Arrange
-				var viewModel = new MainViewModel();
-				string digit = "5";
+			// Arrange
+			var viewModel = new MainViewModel();
+			string digit = "5";
 
-				// Act
-				viewModel.AddSymbol(digit);
+			// Act
+			viewModel.AddSymbol(digit);
 
-				// Assert
-				Assert.AreEqual("5", viewModel.InputText);
-			}
+			// Assert
+			Assert.AreEqual("5", viewModel.InputText);
+		}
+
+		[TestMethod]
+		public void AddSymbol_AddsDecimalPointToRealPart()
+		{
+			// Arrange
+			var viewModel = new MainViewModel();
+			viewModel.InputText = "15";
+
+			// Act
+			viewModel.AddSymbol(".");
+
+			// Assert
+			Assert.AreEqual("15.", viewModel.InputText);
+		}
+
+		[TestMethod]
+		public void AddSymbol_AddsDecimalPointToImaginaryPart()
+		{
+			// Arrange
+			var viewModel = new MainViewModel();
+			viewModel.InputText = "15i2";
+
+			// Act
+			viewModel.AddSymbol(".");
+
+			// Assert
+			Assert.AreEqual("15i2.", viewModel.InputText);
+		}
+
+		[TestMethod]
+		public void Calculate_ClearsInputText()
+		{
+			// Arrange
+			var viewModel = new MainViewModel();
+			viewModel.InputText = "15i2";
+
+			// Act
+			viewModel.Calculate("C");
+
+			// Assert
+			Assert.AreEqual("", viewModel.InputText);
+		}
+
+		[TestMethod]
+		public void Calculate_StoresInMemory()
+		{
+			// Arrange
+			var viewModel = new MainViewModel();
+			viewModel.InputText = "15i2";
+
+			// Act
+			viewModel.Calculate("MIn");
+
+			// Assert
+			Assert.AreEqual("15i2", viewModel.MemoryLabelText);
+		}
+
+		[TestMethod]
+		public void Calculate_RetrievesFromMemory()
+		{
+			// Arrange
+			var viewModel = new MainViewModel();
+			viewModel.MemoryLabelText = "15i2";
+
+			// Act
+			viewModel.Calculate("MOut");
+
+			// Assert
+			Assert.AreEqual("15i2", viewModel.InputText);
+		}
+
+		[TestMethod]
+		public void Calculate_CalculatesModulus()
+		{
+			// Arrange
+			var viewModel = new MainViewModel();
+			viewModel.InputText = "3i4";
+
+			// Act
+			viewModel.Calculate("Mod");
+
+			// Assert
+			Assert.AreEqual("5", viewModel.InputText);
+		}
 
 
-			[TestMethod]
-			public void AddSymbol_AddsDecimalPointToRealPart()
-			{
-				// Arrange
-				var viewModel = new MainViewModel();
-				viewModel.InputText = "15";
+		[TestMethod]
+		public void CheckInput_ValidRealNumber()
+		{
+			// Arrange
+			var viewModel = new MainViewModel();
+			string input = "15.5";
 
-				// Act
-				viewModel.AddSymbol(".");
+			// Act
+			bool result = viewModel.CheckInput(input);
 
-				// Assert
-				Assert.AreEqual("15.", viewModel.InputText);
-			}
-
-			[TestMethod]
-			public void AddSymbol_AddsDecimalPointToImaginaryPart()
-			{
-				// Arrange
-				var viewModel = new MainViewModel();
-				viewModel.InputText = "15i2";
-
-				// Act
-				viewModel.AddSymbol(".");
-
-				// Assert
-				Assert.AreEqual("15i2.", viewModel.InputText);
-			}
-
-			[TestMethod]
-			public void Calculate_ClearsInputText()
-			{
-				// Arrange
-				var viewModel = new MainViewModel();
-				viewModel.InputText = "15i2";
-
-				// Act
-				viewModel.Calculate("C");
-
-				// Assert
-				Assert.AreEqual("", viewModel.InputText);
-			}
+			// Assert
+			Assert.IsTrue(result);
+		}
 
 
+		[TestMethod]
+		public void Calculate_CalculatesArgumentInDegrees()
+		{
+			// Arrange
+			var viewModel = new MainViewModel();
+			viewModel.InputText = "1i1";
 
-			[TestMethod]
-			public void Calculate_StoresInMemory()
-			{
-				// Arrange
-				var viewModel = new MainViewModel();
-				viewModel.InputText = "15i2";
+			// Act
+			viewModel.Calculate("Deg");
 
-				// Act
-				viewModel.Calculate("MIn");
+			// Assert
+			Assert.AreEqual("45", viewModel.InputText);
+		}
 
-				// Assert
-				Assert.AreEqual("15i2", viewModel.MemoryLabelText);
-			}
 
-			[TestMethod]
-			public void Calculate_RetrievesFromMemory()
-			{
-				// Arrange
-				var viewModel = new MainViewModel();
-				viewModel.MemoryLabelText = "15i2";
-
-				// Act
-				viewModel.Calculate("MOut");
-
-				// Assert
-				Assert.AreEqual("15i2", viewModel.InputText);
-			}
-
-			[TestMethod]
-			public void Calculate_CalculatesModulus()
-			{
-				// Arrange
-				var viewModel = new MainViewModel();
-				viewModel.InputText = "3i4";
-
-				// Act
-				viewModel.Calculate("Mod");
-
-				// Assert
-				Assert.AreEqual("5", viewModel.InputText);
-			}
-		
 	}
 }
